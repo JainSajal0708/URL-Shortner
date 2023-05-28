0410bdabe0bd08e178d91f777fb733e5b539807d
Sudhanshu Jain <motabhai0708@gmail.com>
2023-05-28 16:33:05 +0530

    Final project

---
+++ app.js
@@ -0,0 +1,48 @@
+#short-url {
+    display: flex;
+    align-items : center;
+    justify-content : center;
+  }
+  
+  #short-url a {
+    word-wrap: break-word;
+    margin-right: 10px;
+    color: #fff;
+  }
+
+  .maincontainer
+  {
+    width:80%;
+    height: auto;
+    min-height:350px;
+    padding:40px;
+    border-radius: 30px;
+    box-shadow: 0px 0px 11px 4px rgba(0, 0, 0, 0.25);
+    background: #4e54c8;``
+    background: -webkit-linear-gradient(to right, #8f94fb, #4e54c8);  
+    background: linear-gradient(to right, #8f94fb, #4e54c8);
+    margin: auto;
+  }
+
+  #copy-btn{
+    background-color: #fff;
+    color: #8f94fb;
+    border: none;
+    padding: 5px 10px;
+    cursor: pointer;
+    border-radius: 10px;
+    font-size: 25px;
+    margin-top: 20px;
+  }
+  .credit a {
+    text-decoration: none;
+    color: #000;
+    font-weight: 800;
+}
+
+.credit {
+    text-align: center;
+    margin-top: 10px;
+    font-family: Verdana,Geneva,Tahoma,sans-serif;
+}
+
